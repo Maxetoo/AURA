da55e888cc19b698ebe3245c6024949890e18d9f
Kizitolann <etombimaxwell@gmail.com>
2025-05-27 15:11:00 +0100

    assessment fix

--- frontend/src/pages/AssessmentPage.jsx
+++ frontend/src/pages/AssessmentPage.jsx
@@ -1,68 +1,73 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
-import {Link, Navigate, useLocation} from 'react-router-dom';
-// import {useNavigate} from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 import SingleAssessment from '../components/assessment/SingleAssessment';
-import {getAssessments} from '../slices/assessmentSlice';
-import { useSelector, useDispatch} from 'react-redux'
-import {Loader} from '../helpers';
-
- 
-
+import { getAssessments } from '../slices/assessmentSlice';
+import { Loader } from '../helpers';
 
 const AssessmentPage = () => {
-   const dispatch = useDispatch()
-   const {assessments, currentAssessmentAnalysis, currentAssessment, getAssessmentLoad } = useSelector((store) => store.assessment)
-    const {user, isLoading} = useSelector((store) => store.user)
-   const location = useLocation();
+  const dispatch = useDispatch();
 
-   const selectedAssessments = !isLoading && user?.recommendedTests?.find((item) => item._id === currentAssessmentAnalysis)?.assessments || []
-  
-   
-    const activeAssessments = assessments?.length > 0 ? assessments : selectedAssessments
+  const {
+    assessments,
+    currentAssessmentAnalysis,
+    currentAssessment,
+    getAssessmentLoad,
+  } = useSelector((store) => store.assessment);
+  const { user, isLoading } = useSelector((store) => store.user);
+
+  const selectedAssessments =
+    (!isLoading &&
+      user?.recommendedTests?.find(
+        (item) => item._id === currentAssessmentAnalysis
+      )?.assessments) ||
+    [];
+
+  const defaultAssessments =
+    (!isLoading && user?.recommendedTests?.[0]?.assessments) || [];
+
+  const activeAssessments = assessments?.length > 0 ? assessments : selectedAssessments;
 
   useEffect(() => {
-          if (currentAssessmentAnalysis) {
-              dispatch(getAssessments({analysisId: currentAssessmentAnalysis }))
-          }
-  }, [dispatch, currentAssessmentAnalysis]);
+    const defaultAssessmentId = user?.recommendedTests?.[0]?._id;
+    const analysisId = currentAssessmentAnalysis || defaultAssessmentId;
 
-  const {assessment} = currentAssessment || {}
+    if (analysisId) {
+      dispatch(getAssessments({ analysisId }));
+    }
+  }, [dispatch, currentAssessmentAnalysis, user]);
 
-  
+  const { assessment } = currentAssessment || {};
 
-  if (!currentAssessmentAnalysis && activeAssessments.length === 0) {
-    return <Navigate to="/" state={{ from: location }} replace />
-  }
-      
   return (
     <Wrapper>
-      <>
       <div className="secondary_wrapper">
-          <h3 className="title">Recommended Assessments</h3>
-          <p className="desc">
-            Based on your symptoms, our AI recommends the following assessments. These assessments are designed to help evaluate specific mental health conditions.
-          </p>
-          <div className="assessment_container">
-              {
-                (getAssessmentLoad && assessment?.review?.length === 0) ||activeAssessments.length === 0 ? <div className="load_section">
-                  <Loader/>
-                </div> :
-                activeAssessments.map((values) => {
-                  return <SingleAssessment 
-                  {...values} 
+        <h3 className="title">Recommended Assessments</h3>
+        <p className="desc">
+          Based on your symptoms, our AI recommends the following assessments. These assessments are designed to help evaluate specific mental health conditions.
+        </p>
+        <div className="assessment_container">
+          {getAssessmentLoad ? (
+            <div className="load_section">
+              <Loader />
+            </div>
+          ) : (
+            (activeAssessments.length > 0 ? activeAssessments : defaultAssessments).map(
+              (values) => (
+                <SingleAssessment
+                  {...values}
                   key={values._id}
-                  takenTests={assessment?.review || []}
+                  takenTests={(assessment && assessment.review) || []}
                   analysisId={currentAssessmentAnalysis}
-                  />
-                })
-              }
-          </div>
+                />
+              )
+            )
+          )}
         </div>
-      </>
+      </div>
     </Wrapper>
-  )
-}
+  );
+};
 
 const Wrapper = styled.div`
   width: 100%;
--- frontend/src/paths/url.jsx
+++ frontend/src/paths/url.jsx
@@ -1,1 +1,2 @@
 export const URL = 'https://aura-a2kd.onrender.com'
+// export const URL = 'http://localhost:5000'
